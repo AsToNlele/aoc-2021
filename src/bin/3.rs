87f36e50ac4669f4a7d01521f7d0c2778e0152ce
AsToNlele <theastonofficial@gmail.com>
2024-09-29 02:37:53 +0200

    feat: Day 3 part two

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -4,7 +4,8 @@ use aoc_2021::get_lines;
 
 fn main() -> Result<()> {
     let lines = get_lines("inputs/3.txt")?;
-    part_one(&lines);
+    // part_one(&lines);
+    part_two(&lines);
 
     Ok(())
 }
@@ -45,4 +46,55 @@ fn part_one(lines: &[String]) {
 
     let result = gamma_rate as u32 * epsilon_rate as u32;
     println!("{}", result);
+}
+
+fn part_two(lines: &[String]) {
+    let line_length = lines[0].len();
+    let mut new_lines = lines.to_vec();
+    for index in 0..line_length {
+        new_lines = find_occurrences(&new_lines, index, true);
+        if new_lines.len() == 1 {
+            break;
+        }
+    }
+    let oxygen_rating = binary_to_decimal(&new_lines[0]);
+
+    let mut new_lines = lines.to_vec();
+    for index in 0..line_length {
+        new_lines = find_occurrences(&new_lines, index, false);
+        if new_lines.len() == 1 {
+            break;
+        }
+    }
+    let co2_rating = binary_to_decimal(&new_lines[0]);
+
+    let res = oxygen_rating * co2_rating;
+    println!("{:?}", res);
+}
+
+fn find_occurrences(vec: &[String], index: usize, most_occurrent: bool) -> Vec<String> {
+    let (occurrences_of_one, occurrences_of_zero): (Vec<_>, Vec<_>) = vec
+        .iter()
+        .partition(|x| x.chars().nth(index).unwrap() == '1');
+
+    if occurrences_of_one.len() >= occurrences_of_zero.len() {
+        if most_occurrent {
+            occurrences_of_one
+        } else {
+            occurrences_of_zero
+        }
+    } else if most_occurrent {
+        occurrences_of_zero
+    } else {
+        occurrences_of_one
+    }
+    .into_iter()
+    .cloned()
+    .collect()
+}
+
+fn binary_to_decimal(binary: &str) -> u32 {
+    binary
+        .chars()
+        .fold(0, |acc, value| (acc << 1) + value.to_digit(2).unwrap())
 }
